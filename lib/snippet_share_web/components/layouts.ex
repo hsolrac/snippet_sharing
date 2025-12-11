5214bf98760332d09fc5f7ebf0d389b03ee8f169
Carlos Henrique <ch.henriquevinha@gmail.com>
2025-12-11 18:22:04 -0300

    WIP: navigation bar

--- lib/snippet_share_web/components/layouts.ex
+++ lib/snippet_share_web/components/layouts.ex
@@ -35,30 +35,27 @@ defmodule SnippetShareWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <header class="navbar px-4 sm:px-6 lg:px-8">
-      <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+    <header class="flex justify-between items-center px-6 py-3 bg-base-200 h-[50px]">
+      <div class="flex relative">
+        <a href={~p"/"} class="mr-6">
+          <div class="text-white font-bold text-3xl">Snippets</div>
         </a>
+        <div>
+          <input
+            type="text"
+            class="rounded-lg focus:outline-none focus:border-emLavender focus:ring-0 px-3 py-1 bg-emDark-dark placeholder-emDark-light text-white font-brand font-regular text-sm mr-5 border-white"
+            placeholder="Search..."
+          />
+          <button class="mt-2 mr-2 text-white text-[1rem] font-brand font-bold hover:text-emDark-light">
+            All snippets
+          </button>
+        </div>
       </div>
-      <div class="flex-none">
-        <ul class="flex flex-column px-1 space-x-4 items-center">
-          <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
-            <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
-          </li>
-        </ul>
+
+      <div class="relative">
+        <button class="img-down-arror" type="button" id="user-menu-button">
+          <img src="" alt="Profile Image" class="round-image-padding w-8 h-8" />
+        </button>
       </div>
     </header>
 
--- lib/snippet_share_web/live/user_live/login.ex
+++ lib/snippet_share_web/live/user_live/login.ex
@@ -11,52 +11,8 @@ defmodule SnippetShareWeb.UserLive.Login do
         <div class="text-center">
           <.header>
             <p>Log in</p>
-            <:subtitle>
-              <%= if @current_scope do %>
-                You need to reauthenticate to perform sensitive actions on your account.
-              <% else %>
-                Don't have an account? <.link
-                  navigate={~p"/users/register"}
-                  class="font-semibold text-brand hover:underline"
-                  phx-no-format
-                >Sign up</.link> for an account now.
-              <% end %>
-            </:subtitle>
           </.header>
         </div>
-
-        <div :if={local_mail_adapter?()} class="alert alert-info">
-          <.icon name="hero-information-circle" class="size-6 shrink-0" />
-          <div>
-            <p>You are running the local mail adapter.</p>
-            <p>
-              To see sent emails, visit <.link href="/dev/mailbox" class="underline">the mailbox page</.link>.
-            </p>
-          </div>
-        </div>
-
-        <.form
-          :let={f}
-          for={@form}
-          id="login_form_magic"
-          action={~p"/users/log-in"}
-          phx-submit="submit_magic"
-        >
-          <.input
-            readonly={!!@current_scope}
-            field={f[:email]}
-            type="email"
-            label="Email"
-            autocomplete="email"
-            required
-            phx-mounted={JS.focus()}
-          />
-          <.button class="btn btn-primary w-full">
-            Log in with email <span aria-hidden="true">→</span>
-          </.button>
-        </.form>
-
-        <div class="divider">or</div>
 
         <.form
           :let={f}
